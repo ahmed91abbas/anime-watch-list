987b6c16eb4c8217a343f11ea443f2291a8852d3
Ahmed Abbas <ahmed91abbas@hotmail.com>
2021-02-05 23:50:37 +0100

    change look and feel of gui

--- anime_watch_list.py
+++ anime_watch_list.py
@@ -29,7 +29,7 @@ class AnimeWatchListGUI:
         menu.add_cascade(label="Options", menu=options_menu)
         options_menu.add_command(label="Edit the config file", command=self.on_edit_config)
 
-        body_frame = tk.Frame(self.root, bg=bg_color)
+        body_frame = tk.Frame(self.root)
         body_frame.pack(pady=15)
 
         if not config:
@@ -38,9 +38,9 @@ class AnimeWatchListGUI:
             body_frame.config(width=500, height=300)
             return
 
-        self.canvas = tk.Canvas(body_frame, bg=bg_color)
+        self.canvas = tk.Canvas(body_frame, bg=bg_color, bd=0)
         scrollbar = tk.Scrollbar(body_frame, orient="vertical", command=self.canvas.yview)
-        scrollable_frame = tk.Frame(self.canvas, bg=bg_color)
+        scrollable_frame = tk.Frame(self.canvas)
         self.canvas.grid_propagate(False)
         scrollable_frame.bind("<Configure>", lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
         self.canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
@@ -54,20 +54,22 @@ class AnimeWatchListGUI:
         padx = 15
         pady = 5
         title_width = 50
+        component_config = {'height': 2, 'bg': bg_color, 'font': ('calibri', 16)}
         for i, c in enumerate(config):
-            grid_config = {'padx': 10, 'pady': 5, 'row': i}
-            component_config = {'bg': bg_color, 'font': ('calibri', 16)}
+            grid_config = {'pady': pady, 'row': i}
             title_label = tk.Label(scrollable_frame, text=self.trim_text(c['title'], title_width), **component_config, width=title_width)
             title_label.grid(**grid_config, column=0)
-            ep_label = tk.Label(scrollable_frame, text=f'#{c["ep"]}', **component_config, width=5)
+            ep_label = tk.Label(scrollable_frame, text=f'#{c["ep"]}', **component_config, width=5, anchor="e")
             ep_label.grid(**grid_config, column=1)
-            button = tk.Button(scrollable_frame, text="Watch next ep", **component_config, highlightthickness=1, activebackground=bg_color,\
-                state='normal' if c['next_ep_url'] else 'disabled', compound=tk.CENTER, command=partial(self.on_open_page, i, c['next_ep_url']))
-            button.grid(**grid_config, column=2)
+            button = tk.Button(scrollable_frame, text="Watch next ep", bg='#f7e4d0', font=component_config['font'], highlightthickness=2,\
+                activebackground=bg_color, compound=tk.CENTER, command=partial(self.on_open_page, i, c['next_ep_url']))
+            if not c['next_ep_url']:
+                button.config(state='disabled', highlightthickness=0)
+            button.grid(**grid_config, padx=padx, column=2)
 
         self.canvas.update_idletasks()
-        frame_width = title_label.winfo_width() + ep_label.winfo_width() + button.winfo_width() + grid_config['padx'] * 6
-        frame_height = (button.winfo_height() + grid_config['pady'] * 2) * max_row_count
+        frame_width = title_label.winfo_width() + ep_label.winfo_width() + button.winfo_width() + padx * 2
+        frame_height = (button.winfo_height() + pady * 2) * max_row_count
         self.canvas.config(width=frame_width, height=frame_height)
 
     def on_close(self):
