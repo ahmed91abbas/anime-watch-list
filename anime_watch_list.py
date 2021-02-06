fa56cf4b64b9562f59cf5a2f2180e7bbe9eacb7a
Ahmed Abbas <ahmed91abbas@hotmail.com>
2021-02-06 09:26:29 +0100

    change gui look and feel

--- anime_watch_list.py
+++ anime_watch_list.py
@@ -16,9 +16,11 @@ class AnimeWatchListGUI:
 
     def createGUI(self, config, max_row_count=10):
         bg_color = '#e6e6ff'
+        secondary_color = '#b28fc7'
+        button_color = '#f7e4d0'
 
         self.root = tk.Tk()
-        self.root.configure(background=bg_color)
+        self.root.configure(background=secondary_color)
         self.root.title("Anime Watch List")
         self.root.wm_protocol("WM_DELETE_WINDOW", self.on_close)
         self.root.resizable(False, False)
@@ -29,23 +31,23 @@ class AnimeWatchListGUI:
         menu.add_cascade(label="Options", menu=options_menu)
         options_menu.add_command(label="Edit the config file", command=self.on_edit_config)
 
-        body_frame = tk.Frame(self.root)
-        body_frame.pack(pady=15)
+        body_frame = tk.Frame(self.root, bg=secondary_color)
+        body_frame.pack()
 
         if not config:
             body_frame.grid_propagate(False)
-            tk.Label(body_frame, text="No content found in config.txt", bg=bg_color, font=('calibri', 22)).grid()
+            tk.Label(body_frame, text="No content found in config.txt", bg=secondary_color, font=('calibri', 22)).grid()
             body_frame.config(width=500, height=300)
             return
 
-        self.canvas = tk.Canvas(body_frame, bg=bg_color, bd=0)
+        self.canvas = tk.Canvas(body_frame, bd=0, highlightthickness=0)
         scrollbar = tk.Scrollbar(body_frame, orient="vertical", command=self.canvas.yview)
-        scrollable_frame = tk.Frame(self.canvas)
+        scrollable_frame = tk.Frame(self.canvas, bg=secondary_color)
         self.canvas.grid_propagate(False)
         scrollable_frame.bind("<Configure>", lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
         self.canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
         self.canvas.configure(yscrollcommand=scrollbar.set)
-        self.canvas.pack(side="left", fill="both", expand=True)
+        self.canvas.pack(side="left", fill="both", expand=True, pady=5)
 
         if len(config) > max_row_count:
             scrollbar.pack(side="right", fill="y")
@@ -59,9 +61,9 @@ class AnimeWatchListGUI:
             grid_config = {'pady': pady, 'row': i}
             title_label = tk.Label(scrollable_frame, text=self.trim_text(c['title'], title_width), **component_config, width=title_width)
             title_label.grid(**grid_config, column=0)
-            ep_label = tk.Label(scrollable_frame, text=f'#{c["ep"]}', **component_config, width=5, anchor="e")
+            ep_label = tk.Label(scrollable_frame, text=f'#{c["ep"]}', **component_config, width=5, anchor="w")
             ep_label.grid(**grid_config, column=1)
-            button = tk.Button(scrollable_frame, text="Watch next ep", bg='#f7e4d0', font=component_config['font'], highlightthickness=2,\
+            button = tk.Button(scrollable_frame, text="Watch next ep", bg=button_color, font=component_config['font'], highlightthickness=2,\
                 activebackground=bg_color, compound=tk.CENTER, command=partial(self.on_open_page, i, c['next_ep_url']))
             if not c['next_ep_url']:
                 button.config(state='disabled', highlightthickness=0)
