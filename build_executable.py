4b5ab4b27c395aefa625069104593571747f2aed
Ahmed Abbas <ahmed91abbas@hotmail.com>
2022-05-20 22:26:33 +0200

    switch to flat dist for faster start up

--- build_executable.py
+++ build_executable.py
@@ -10,17 +10,20 @@ def path_join(list_of_names):
         path = os.path.join(path, name)
     return path
 
+
 def remove_file(file):
     try:
         os.remove(file)
     except:
         pass
 
+
 def remove_dir(dir):
     try:
         shutil.rmtree(dir)
     except:
         pass
+
 
 def clean_up():
     remove_file('anime_watch_list.spec')
@@ -29,18 +32,17 @@ def clean_up():
     for dir_ in dirs:
         remove_dir(dir_)
 
+
 def call_process(root_path):
     site_packages = path_join([root_path, 'env', 'Lib', 'site-packages'])
     params = ['pyinstaller',
-              '--onefile',
               '--noconsole',
               '--paths', site_packages,
+              '--hidden-import', 'six',
               '--add-data', 'images/image-not-found.png;images',
+              '--add-data', 'config.txt;.',
               'anime_watch_list.py']
     subprocess.call(params)
-
-def add_files(root_path):
-    shutil.copyfile('config.txt', path_join([root_path, 'dist', 'config.txt']))
 
 
 if __name__ == '__main__':
@@ -49,6 +51,4 @@ if __name__ == '__main__':
     clean_up()
     print('\nCreating the exe...\n')
     call_process(root_path)
-    print('\nAdding missing files...\n')
-    add_files(root_path)
     print('\nDone.')
